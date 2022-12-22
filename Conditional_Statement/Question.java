b026fe311b47e5b4fd363861c10f9092605eca70
Krishna <110826354+ifastprasad@users.noreply.github.com>
2022-12-22 09:00:37 +0530

    i hava write chake odd and even number function

--- Conditional_Statement/Question.java
+++ Conditional_Statement/Question.java
@@ -1,7 +1,20 @@
 package Conditional_Statement;
 
-public class Question {
-    public static void main(String arg[]) {
+import java.util.Scanner;
 
+public class Question {
+    public static void odd_even() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter youu number = ");
+        int a = sc.nextInt();
+        if(a % 2 == 0) {
+            System.out.println("even");
+        }else {
+            System.out.println("odd");
+        }
+        sc.close();
+    }
+    public static void main(String arg[]) {
+        odd_even();
     }
 }
