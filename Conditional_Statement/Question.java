0a38ef31dbbcbfbcfd5922ed5ebfa0b9beba0893
Krishna <110826354+ifastprasad@users.noreply.github.com>
2022-12-26 22:56:27 +0530

    dleted loop in one fun

--- Conditional_Statement/Question.java
+++ Conditional_Statement/Question.java
@@ -99,6 +99,24 @@ public class Question {
         }
         sc.close();
     }
+    public static void print_lar_3num() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your fir Num = ");
+        int a = sc.nextInt();
+        System.out.print("Enter your sec Num = ");
+        int b = sc.nextInt();
+        System.out.print("Enter your thi Num = ");
+        int c = sc.nextInt();
+
+        if(a >= b && a >= c) {
+            System.out.println( a +" Is a grater");
+        }else if(b >= c) {
+            System.out.println(b + " Is a grater");
+        }else{
+            System.out.println(c + " Is a grater");
+        }
+        sc.close();
+    }
     public static void main(String arg[]) {
         // odd_even();
         // largesof2Number();
