0cc943005902c3bfe29d02dbb2f6b03350b55cf1
Krishna <110826354+ifastprasad@users.noreply.github.com>
2022-12-27 13:23:06 +0530

    complited patteran part-1

--- Patterns_part1/mvp.java
+++ Patterns_part1/mvp.java
@@ -13,8 +13,48 @@ public class mvp {
             }
             System.out.println();
         }
+        sc.close();
+    }
+    public static void invartedStar() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your numner = ");
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i+1; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    public static void halfPira() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enge your number = ");
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++) {
+            for(int j = 1; j<=i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void charPira() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter your number = ");
+        int n = sc.nextInt();
+        char c = 'A';
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print(c+" ");
+                c++;
+            }
+            System.out.println();
+        }
     }
     public static void main(String[] args) {
-        printnStar();
+        // printnStar();
+        // invartedStar();
+        // halfPira();
+        charPira();
     }
 }
